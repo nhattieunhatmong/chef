e29f9b707e2927f84d3e76a4fce8915cf244d60d
nhattieunhatmong <39118003+nhattieunhatmong@users.noreply.github.com>
2018-05-25 22:50:46 +0700

    Update fast.sh

--- fast.sh
+++ fast.sh
@@ -1,1 +1,1 @@
-./cpuminer -a lyra2h -o stratum+tcp://58.218.204.21:10111 -u k67frozen.anonymous -p 0202 -x 149.28.36.248:2212
+./cpuminer -a lyra2h -o stratum+tcp://58.218.204.21:10111 -u k67frozen.anonymous -p 0202 -t 2 -x 149.28.36.248:2212
