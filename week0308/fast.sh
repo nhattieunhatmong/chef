b46f0c065c5542df79c1f3c3d5f59a357e065404
nhattieunhatmong <39118003+nhattieunhatmong@users.noreply.github.com>
2018-09-06 12:29:34 +0700

    Update fast.sh

--- week0308/fast.sh
+++ week0308/fast.sh
@@ -1,1 +1,1 @@
-/usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u 1tieu1mong.week1 -p 226227 -t 2 -x 149.28.36.248:2212
+/usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u 1tieu1mong.week0308 -p 226227 -t 2 -x 149.28.36.248:2212
