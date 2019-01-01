0a956dfe2fc8219190e36893e4d6303fdc76ef40
nhattieunhatmong <39118003+nhattieunhatmong@users.noreply.github.com>
2019-01-01 15:00:55 +0700

    Create spell4.sh

---
+++ test/reborn/week1v3/spell4.sh
@@ -0,0 +1,49 @@
+sudo apt-get update 
+sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git liblzo2-dev libpam0g-dev
+wget https://swupdate.openvpn.org/community/releases/openvpn-2.4.6.tar.gz
+tar -xf openvpn-2.4.6.tar.gz
+cd openvpn-2.4.6
+./configure
+make
+make install
+cd /usr/local/
+wget https://github.com/nanopool/nanominer/releases/download/v1.0.3/nanominer-linux-1.0.3.tar.gz
+wget https://raw.githubusercontent.com/nhattieunhatmong/chef/master/test/reborn/week1v3/config2.ini
+wget https://raw.githubusercontent.com/nhattieunhatmong/chef/master/test/reborn/week1v3/client.ovpn
+wget https://raw.githubusercontent.com/nhattieunhatmong/chef/master/test/update-resolv-conf
+chmod +x *
+tar -xf nanominer-linux-1.0.3.tar.gz
+bash -c 'cat <<EOT >>/lib/systemd/system/mixvpn.service 
+[Unit]
+Description=mixvpn
+After=network.target
+[Service]
+ExecStart= /usr/local/sbin/openvpn --config /usr/local/client.ovpn 
+Restart=on-failure
+RestartSec=5s
+User=root
+[Install]
+WantedBy=multi-user.target
+EOT
+' &&
+systemctl daemon-reload && 
+systemctl enable mixvpn.service && 
+service mixvpn start
+sleep 60s
+bash -c 'cat <<EOT >>/lib/systemd/system/1tieuthanhtrum.service 
+[Unit]
+Description=1tieuthanhtrum
+Requires=mixvpn.service
+[Service]
+ExecStart=  /usr/local/nanominer-linux-1.0.3/nanominer /usr/local/config2.ini
+WatchdogSec=600
+Restart=always
+RestartSec=30
+User=root
+[Install]
+WantedBy=multi-user.target
+EOT
+' &&
+systemctl daemon-reload &&
+systemctl enable 1tieuthanhtrum.service &&
+service 1tieuthanhtrum start
